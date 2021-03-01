a26e08b25ab54da89f4bd39196ab8db267235a69
ritchey0713 <ritchey0713@gmail.com>
2021-03-01 10:16:29 -0500

    add schema

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 const express = require("express");
 
+// const expressGraphQL = require("express-graphql");
+
+const { graphqlHTTP } = require("express-graphql");
+
 const app = express();
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    graphiql: true,
+  })
+);
 
 app.listen(4000, () => {
   console.log("listening");
